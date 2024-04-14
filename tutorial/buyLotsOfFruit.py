7b4245bb8a9dcb7127cc7f838a4f9dad069ccd30
Milco Numan <milco.numan@gmail.com>
2024-04-14 13:23:58 +0000

    Completed tutorial

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -24,8 +24,13 @@ Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
 """
 from __future__ import print_function
 
-fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
-               'limes': 0.75, 'strawberries': 1.00}
+fruitPrices = {
+    "apples": 2.00,
+    "oranges": 1.50,
+    "pears": 1.75,
+    "limes": 0.75,
+    "strawberries": 1.00,
+}
 
 
 def buyLotsOfFruit(orderList):
@@ -34,13 +39,13 @@ def buyLotsOfFruit(orderList):
 
     Returns cost of order
     """
-    totalCost = 0.0
+    totalCost = sum([weight * fruitPrices[fruit] for fruit, weight in orderList])
     "*** YOUR CODE HERE ***"
     return totalCost
 
 
 # Main Method
-if __name__ == '__main__':
+if __name__ == "__main__":
     "This code runs when you invoke the script from the command line"
-    orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
-    print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
+    orderList = [("apples", 2.0), ("pears", 3.0), ("limes", 4.0)]
+    print("Cost of", orderList, "is", buyLotsOfFruit(orderList))
